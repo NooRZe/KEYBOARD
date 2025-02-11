bf0f8376e902b59dffb174940ac7478f32ddd33d
NooRZe <noorze9@gmail.com>
2025-02-11 22:40:37 +0300

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,113 @@
+export const buttons = [{
+  buttonSymbol: 'q',
+  buttonId: 1,
+},{
+  buttonSymbol: 'w',
+  buttonId: 2,
+},{
+  buttonSymbol: 'e',
+  buttonId: 3,
+},{
+  buttonSymbol: 'r',
+  buttonId: 4,
+},{
+  buttonSymbol: 't',
+  buttonId: 5,
+},{
+  buttonSymbol: 'y',
+  buttonId: 6,
+},{
+  buttonSymbol: 'u',
+  buttonId: 7,
+},{
+  buttonSymbol: 'i',
+  buttonId: 8,
+},{
+  buttonSymbol: 'o',
+  buttonId: 9,
+},{
+  buttonSymbol: 'p',
+  buttonId: 10,
+},{
+  buttonSymbol: '[',
+  buttonId: 11,
+},{
+  buttonSymbol: ']',
+  buttonId: 12,
+},{
+  buttonSymbol: 'a',
+  buttonId: 13,
+},{
+  buttonSymbol: 's',
+  buttonId: 14,
+},{
+  buttonSymbol: 'd',
+  buttonId: 15,
+},{
+  buttonSymbol: 'f',
+  buttonId: 16,
+},{
+  buttonSymbol: 'g',
+  buttonId: 17,
+},{
+  buttonSymbol: 'h',
+  buttonId: 18,
+},{
+  buttonSymbol: 'j',
+  buttonId: 19,
+},{
+  buttonSymbol: 'k',
+  buttonId: 20,
+},{
+  buttonSymbol: 'l',
+  buttonId: 21,
+},{
+  buttonSymbol: ';',
+  buttonId: 22,
+},{
+  buttonSymbol: '"',
+  buttonId: 23,
+},{
+  buttonSymbol: '|',
+  buttonId: 24,
+},{
+  buttonSymbol: 'z',
+  buttonId: 25,
+},{
+  buttonSymbol: 'x',
+  buttonId: 26,
+},{
+  buttonSymbol: 'c',
+  buttonId: 27,
+},{
+  buttonSymbol: 'v',
+  buttonId: 28,
+},{
+  buttonSymbol: 'b',
+  buttonId: 29,
+},{
+  buttonSymbol: 'n',
+  buttonId: 30,
+},{
+  buttonSymbol: 'm',
+  buttonId: 31,
+},{
+  buttonSymbol: ',',
+  buttonId: 32,
+},{
+  buttonSymbol: '.',
+  buttonId: 33,
+},{
+  buttonSymbol: '/',
+  buttonId: 34,
+}];
+
+
+export const exeptions = ['Shift', 'Tab', 'Escape', 'Control', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'CapsLock', 'Enter', 'ScrollLock', 'Pause', 'NumLock', 'Meta','Alt', 'AltGraph', 'ContextMenu','ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown', 'Insert', 'Home', 'PageUp', 'Delete', 'End', 'PageDown'];
+
+ export const exeptions1 = {
+  "Shift": "" , 
+  "Tab": "" , 
+  "Escape": "" ,
+} 
+
---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+import { buttons } from "./data.js";
+import { exeptions } from "./data.js";
+import { exeptions1 } from "./data.js";
+
+let buttonHTML = '';
+let keyboardIds = [];
+let resultId = 0;
+
+
+buttons.forEach((button) =>{
+  buttonHTML = buttonHTML +  `
+  <button class="js-button-${button.buttonId}" id="${button.buttonId}">${button.buttonSymbol}</button>
+  `;
+  document.querySelector('.js-keyboard-main').innerHTML = buttonHTML
+  resultId ++;
+  keyboardIds.push(resultId);
+});
+ 
+
+
+//генерация вводимого текста 
+
+let inputResult = [];
+document.body.addEventListener('keydown', (event) => {
+  let input = '';
+  input = event.key;
+  /*switch(input) {
+    case eventExeption(input):
+      break; 
+    case 'Backspace':
+      inputRemove();
+      break
+    default:
+      exeptions1[input] ?? inputFieldgen(input);
+      if (exeptions1[input] === undefined) {
+        inputFieldgen(input);
+      } 
+    };*/
+    if (input === 'Backspace') {
+      inputRemove();
+    } else if (exeptions1[input] === undefined) {
+      inputFieldgen(input);
+    } 
+  })
+
+
+    
+  
+//генерация текста 
+function inputFieldgen (input) {
+  inputResult.push(input);
+      document.querySelector('.js-input-field').innerHTML = '';
+    for (let i=0; i < inputResult.length; i++) {
+      document.querySelector('.js-input-field').innerHTML += inputResult[i]; 
+    };
+}
+
+//удаление
+function inputRemove () {
+  inputResult.splice(inputResult.length-1);
+      document.querySelector('.js-input-field').innerHTML = '';
+    for (let i=0; i < inputResult.length; i++) {
+      document.querySelector('.js-input-field').innerHTML += inputResult[i]; 
+    }; 
+}
+
+//исключения 
+/*
+function eventExeption(input) {
+  for (let i=0; i < exeptions.length; i++) {
+    if (input === exeptions[i]) {
+      return input;
+    }     
+  };
+}
+*/
